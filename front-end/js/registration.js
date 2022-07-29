ed845ac66d52eeaa634377b16c64eca7ed668b19
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-07-28 21:23:22 -0300

    X-Sky BETA - Pushing the front-end

--- front-end/js/registration.js
+++ front-end/js/registration.js
@@ -1,7 +1,13 @@
+/* ------ Function to make the code sleep in milisseconds ------ */
+function Sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/*========================================================
+                    Main Function
+========================================================*/
 (function ($) {
     'use strict';
-    /*==================================================================
-        [ Daterangepicker ]*/
     try {
         $('.js-datepicker').daterangepicker({
             "singleDatePicker": true,
@@ -47,8 +53,6 @@
     
     
     } catch(er) {console.log(er);}
-    /*[ Select 2 Config ]
-        ===========================================================*/
     
     try {
         var selectSimple = $('.js-select-simple');
@@ -67,61 +71,31 @@
     }
 })(jQuery);
 
-/* ---- Submit form ----- */
-
-// Get the form element by id
-const sampleForm = document.getElementById("signup-form");
-
-// Add an event listener to the form element and handler for the submit an event.
-sampleForm.addEventListener("submit", async (e) => {
-  /*Prevent the default browser behaviour of submitting
-   the form so that you can handle this instead.*/
-  e.preventDefault();
-
-  // Get the element attached to the event handler.
-  let form = e.currentTarget;
-
-  // Take the URL from the form's `action` attribute.
-  let url = form.action;
-
-  try {
-    /*Takes all the form fields and make the field values
-    available through a `FormData` instance.*/
-    let formData = new FormData(form);
-
-    
-    // The `postFormFieldsAsJson()` function in the next step.
-    let responseData = await postFormFieldsAsJson({ url, formData });
-
-    //Destructure the response data
-    let { serverDataResponse } = responseData;
-
-    //Display the response data in the console (for debugging)
-    console.log(serverDataResponse);
-  } catch (error) {
-    //If an error occurs display it in the console (for debugging)
-    console.error(error);
-  }
-});
-
-
-// Helper function to POST data as JSON with Fetch.
+/*========================================================
+                    Submit Form
+========================================================*/
 async function postFormFieldsAsJson({url, formData}) {
   //Create an object from the form data entries
   let formDataObject = Object.fromEntries(formData.entries());
+  //Enconde image in base64
+  var reader = new FileReader()
+  reader.readAsDataURL(formDataObject.perfil_photo)
+  reader.onload = function () {
+    formDataObject.perfil_photo = reader.result;
+  };
+  reader.onerror = function (error) {
+    console.log('Error: ', error);
+  };
+  // Sleep function to wait for the above functions to be executed
+  await Sleep(500)
   // Format the plain form data as JSON
-  let formDataJsonString = JSON.stringify(formDataObject);
-
-  //Set the fetch options (headers, body)
+  let formDataJsonString = JSON.stringify(formDataObject);  // Set the fetch options (headers, body)
   let fetchOptions = {
-    //HTTP method set to POST.
     method: "POST",
-    //Set the headers that specify you're sending a JSON body request and accepting JSON response
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    // POST request body as JSON string.
     body: formDataJsonString,
   };
 
@@ -138,3 +112,51 @@ async function postFormFieldsAsJson({url, formData}) {
   window.location.replace("/login/login.html");
   return res.json();
 }
+
+// Get the form element by id
+const sampleForm = document.getElementById("signup-form");
+
+// Add an event listener to the form element and handler for the submit an event.
+sampleForm.addEventListener("submit", async (e) => {
+  /*Prevent the default browser behaviour of submitting
+   the form so that you can handle this instead.*/
+  e.preventDefault();
+
+  // Get the element attached to the event handler.
+  let form = e.currentTarget;
+  // Take the URL from the form's `action` attribute.
+  let url = form.action;
+
+  try {
+    /*Takes all the form fields and make the field values
+    available through a `FormData` instance.*/
+    let formData = new FormData(form);
+    formData.set.name.perfil
+    
+    // The `postFormFieldsAsJson()` function in the next step.
+    let responseData = await postFormFieldsAsJson({ url, formData });
+
+    //Destructure the response data
+    let { serverDataResponse } = responseData;
+
+    //Display the response data in the console (for debugging)
+    console.log(serverDataResponse);
+  } catch (error) {
+    //If an error occurs display it in the console (for debugging)
+    console.error(error);
+  }
+});
+
+/*========================================================
+                    Select Button
+========================================================*/
+$("#selectfile").bind('change',function(){
+    var filename = $("#selectfile").val();
+    if(/^s*$/.test(filename)){
+        $("#blankFile").text("No File Chosen..");
+        $(".success").hide();
+    }else{
+        $("#blankFile").text(filename.replace("C:\\fakepath\\",""));
+        $(".success").show();
+    }
+})
