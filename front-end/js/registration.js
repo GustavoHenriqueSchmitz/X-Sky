9229dba39a4e8ee414d330edd5430db1dd34b795
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-07-31 10:50:21 -0300

    Fixing IDs and Classes

--- front-end/js/registration.js
+++ front-end/js/registration.js
@@ -154,9 +154,9 @@ $("#selectfile").bind('change',function(){
     var filename = $("#selectfile").val();
     if(/^s*$/.test(filename)){
         $("#blankFile").text("No File Chosen..");
-        $(".success").hide();
+        $("#success").hide();
     }else{
         $("#blankFile").text(filename.replace("C:\\fakepath\\",""));
-        $(".success").show();
+        $("#success").show();
     }
 })
