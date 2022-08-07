be616835a45fd2515ba59a7dcf13b9d3c9f1d3a5
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-08-07 14:37:05 -0300

    Finalizing

--- back-end/controllers/login_controllers.go
+++ back-end/controllers/login_controllers.go
@@ -12,18 +12,19 @@ func Login(router *fiber.Ctx) error {
 	loginData := models.Login{}
 
 	if err := router.BodyParser(&loginData); err != nil {
-		return router.SendStatus(500)
+		router.Redirect("http://localhost:3000/login")
+		return nil
 	}
 
 	err := services.Login(loginData)
 
 	if err != nil {
-		router.Redirect("http://localhost:5500/login")
-		return router.Status(403).SendString(err.Error())
+		router.Redirect("http://localhost:3000/login")
+		return nil
 	}
 
-	router.Redirect("http://localhost:5500")
-	return router.SendStatus(302)
+	router.Redirect("http://localhost:3000")
+	return nil
 }
 
 func SignUp(router *fiber.Ctx) error {
@@ -31,14 +32,17 @@ func SignUp(router *fiber.Ctx) error {
 	signData := models.SignUp{}
 
 	if err := router.BodyParser(&signData); err != nil {
-		return router.SendStatus(500)
+		router.Redirect("http://localhost:3000/sign-up")
+		return nil
 	}
 
 	err := services.SignUp(signData)
 
 	if err != nil {
-		return router.Status(400).SendString(err.Error())
+		router.Redirect("http://localhost:3000/sign-up")
+		return nil
 	}
 
-	return router.Status(200).SendString("Success")
+	router.Redirect("http://localhost:3000/login")
+	return nil
 }
